62cdd2b7c95445b4324cd0fb322f822207268e5d
prakhar-ktyr <katiyar.4@iitj.ac.in>
2026-05-16 02:37:29 +0530

    feat: add support for --version and -v flags to display current package version

--- bin/mdview.js
+++ bin/mdview.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const { renderMarkdown } = require('../dist/index.js');
+const pkg = require('../package.json');
 
 // Get the file path from the command line arguments
 const args = process.argv.slice(2);
@@ -10,6 +11,11 @@ const args = process.argv.slice(2);
 if (args.length === 0 || args[0] === '--help' || args[0] === '-h') {
   console.log('Usage: mdview <file.md>');
   process.exit(args.length === 0 ? 1 : 0);
+}
+
+if (args[0] === '--version' || args[0] === '-v') {
+  console.log(`mdview v${pkg.version}`);
+  process.exit(0);
 }
 
 const filePath = path.resolve(process.cwd(), args[0]);
